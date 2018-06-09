722dc5b2b617f253c577fa1cd60773d3a6340153
pr0t <prot@prot.com.pl>
2018-06-09 09:20:23 +0200

    Init commit
    Read to implement

--- Accumulate-Git/Program.cs
+++ Accumulate-Git/Program.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+// Added
+using Accumulate_Git.Helpers;               // AccumulateExtensions
 
 namespace Accumulate_Git
 {
